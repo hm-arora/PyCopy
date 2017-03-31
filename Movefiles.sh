8c4f447e72d2a03de37216132c38baa7726a2228
himanshu <iamhimanshuarora97@gmail.com>
2017-03-31 20:20:06 +0530

    files added

---
+++ Movefiles.sh
@@ -0,0 +1,10 @@
+IFS_BAK=$IFS
+IFS="
+"
+string=""
+for line in $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS; do
+        string="$string  $line"
+        # notify-send $line
+done
+full_path="/home/"$USER"/Desktop/"ds.py
+python $full_path $string
